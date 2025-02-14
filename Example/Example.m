50d48bf94efe0e306686a44f1dddf5627a20b4c2
m-altthaler <108487905+m-altthaler@users.noreply.github.com>
2025-02-14 12:29:18 -0800

    Example script
    --

---
+++ Example/Example.m
@@ -0,0 +1,26 @@
+% run the first block to start the app
+% run any block to open the number of figures.
+
+%% start app
+arrangeFigApp()
+
+%% open 3 blank figures
+for n = 1:3
+    figure(n)
+end
+
+%% open 5 blank figures
+for n = 1:5
+    figure(n)
+end
+
+%% open 15 blank figures
+for n = 1:15
+    figure(n)
+end
+
+%% open 50 blank figures
+for n = 1:50
+    figure(n)
+end
+
